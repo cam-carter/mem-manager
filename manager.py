f24bf29553d83f39ec2fea9cd66fb078fe4f24fe
Todd Loupe <2tmlsbm@gmail.com>
2018-05-02 22:11:01 -0500

    added brainstorm to the loop

--- manager.py
+++ manager.py
@@ -35,6 +35,12 @@ i = 0
 while True:
     jobs[i].pages_occupied = math.ceil(jobs[i].memory_requested / 512)
     if jobs[i].memory_requested <= page_size:
+        # here: if hasDealloc, then set frame counter to 0
+        #if the frame already exists, then skip over it
+        #for j in range(0, number_of_jobs): # checks every job
+        #    if jobs[j].frame_number == frame_counter: # if a frame number exists and it equals the frame counter,
+        #                                              # skip over the amount of pages it occupied
+        #        frame_counter += jobs[j].pages_occupied
         jobs[i].frame_number = frame_counter
     i += 1
     frame_counter += 1
